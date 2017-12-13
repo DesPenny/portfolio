faeb0ed72f93c0fe340cecc6a1df7309929c2b49
Des Penny <desmond.penny@ematicsolutions.com>
2017-12-13 10:05:48 +0000

    lamda function updated

--- upload_portfolio_lamda.py
+++ upload_portfolio_lamda.py
@@ -3,16 +3,27 @@ from botocore.client import Config
 import StringIO
 import zipfile
 
-s3 = boto3.resource('s3', config=Config(signature_version-'s3v4'))
+def lambda_handler(event, context):
+	sns = boto3.resource('sns')
+	topic = sns.Topic('arn:aws:sns:us-east-1:384512602151:deployPortfolioTopic')
+	try:
+	    s3 = boto3.resource('s3', config=Config(signature_version-'s3v4'))
 
-portfolio_bucket = s3.Bucket('portfolio.despenny.rocks')
-build_bucket = s3.Bucket('portfoliobuild.despenny.rocks')
+	    portfolio_bucket = s3.Bucket('portfolio.despenny.rocks')
+	    build_bucket = s3.Bucket('portfoliobuild.despenny.rocks')
+	    
+	    portfolio_zip = StringIO.StringIO()
+	    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
-portfolio_zip = StringIO.StringIO()
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+		with zipfile.ZipFile(portfolio_zip) as myzip:
+			for nm in myzip.namelist():
+				obj = myzip.open(nm)
+				portfolio_bucket.upload_fileonj(obj, nm, ExtraArgs={'ContentType':mimetypes_guesstype(nm)[0]})
+				portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+		print "Deployed!"
 
-	with zipfile.ZipFile(portfolio_zip) as myzip:
-		for nm in myzip.namelist():
-			obj = myzip.open(nm)
-			portfolio_bucket.upload_fileonj(obj, nm, ExtraArgs={'ContentType':mimetypes_guesstype(nm)[0]})
-			portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+		topic.publish(Subject="Portfolio deployed", Message="Portfolio Deployed Successfully!")
+	except:
+    	topic.publish(Subject="Portfolio deploy Failed", Message="Portfolio failed to deploy!")
+    	raise
+    return "Hello from Lambda"
