217cb1aa5e2f41bedf47fef2a7088510c178df30
Des Penny <desmond.penny@ematicsolutions.com>
2018-02-13 17:38:00 +0000

    removed tests temporarily

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ import ExampleWork from './example-work';
 const myWork = [
 	{
 		'title': "Work Example",
+		'href': "https://despenny.rocks",
+		'desc': "Lorem ipsum yadayadayada",
 		'image': {
 			'desc': "example screenshot of a project involving code",
 			'src': "images/example1.png",
@@ -13,6 +15,8 @@ const myWork = [
 	},
 	{
 		'title': "Work Example",
+		'href': "https://despenny.rocks",
+		'desc': "Lorem ipsum yadayadayada",
 		'image': {
 			'desc': "example screenshot of a project involving chemistry",
 			'src': "images/example2.png",
@@ -21,6 +25,8 @@ const myWork = [
 	},
 	{
 		'title': "Work Example",
+		'href': "https://despenny.rocks",
+		'desc': "Lorem ipsum yadayadayada",
 		'image': {
 			'desc': "example screenshot of a project involving cats",
 			'src': "images/example3.png",
