4d5ca97ba518e456ed565c62b32fa93a8bc1762b
Des Penny <desmond.penny@ematicsolutions.com>
2018-02-14 12:38:50 +0000

    adding resume again

--- upload_portfolio_lamda.py
+++ upload_portfolio_lamda.py
@@ -26,14 +26,14 @@ def lambda_handler(event, context):
 
 		portfolio_bucket = s3.Bucket('portfolio.despenny.rocks')
 		build_bucket = s3.Bucket(location["bucketName"])
-		
+
 		portfolio_zip = StringIO.StringIO()
 		build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
 
 		with zipfile.ZipFile(portfolio_zip) as myzip:
 			for nm in myzip.namelist():
 				obj = myzip.open(nm)
-				portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]})
+				portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
 				portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
 		print "Deployed!"
 
