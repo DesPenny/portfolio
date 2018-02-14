760d68904237a91a9535b12059d60b1bea454e4b
Des Penny <desmond.penny@ematicsolutions.com>
2018-02-14 10:14:38 +0000

    tidying lambda to see where bug is

--- upload_portfolio_lamda.py
+++ upload_portfolio_lamda.py
@@ -1,6 +1,7 @@
 import boto3
 from botocore.client import Config
 import StringIO
+import mimetypes
 import zipfile
 
 def lambda_handler(event, context):
@@ -18,19 +19,21 @@ def lambda_handler(event, context):
 			for artifact in  job["data"]["inputArtifacts"]:
 				if artifact["name"] == "MyAppBuild":
 					location = artifact["location"]["s3Location"]
+
 		print "Building Portfolio from "+ str(location)
 
-	    s3 = boto3.resource('s3', config=Config(signature_version-'s3v4'))
-		portfolio_bucket = s3.Bucket('portfolio.despenny.rocks')
-	    build_bucket = s3.Bucket(location["bucketName"])
+		s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-	    portfolio_zip = StringIO.StringIO()
-	    build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
+		portfolio_bucket = s3.Bucket('portfolio.despenny.rocks')
+		build_bucket = s3.Bucket(location["bucketName"])
+		
+		portfolio_zip = StringIO.StringIO()
+		build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
 
 		with zipfile.ZipFile(portfolio_zip) as myzip:
 			for nm in myzip.namelist():
 				obj = myzip.open(nm)
-				portfolio_bucket.upload_fileonj(obj, nm, ExtraArgs={'ContentType':mimetypes_guesstype(nm)[0]})
+				portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]})
 				portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
 		print "Deployed!"
 
@@ -39,6 +42,6 @@ def lambda_handler(event, context):
 			codepipeline = boto3.client('codepipeline')
 			codepipeline.put_job_success_result(jobId=job["id"])
 	except:
-    	topic.publish(Subject="Portfolio deploy Failed", Message="Portfolio failed to deploy!")
-    	raise
-    return "Hello from Lambda"
+		topic.publish(Subject="Portfolio deploy Failed", Message="Portfolio failed to deploy!")
+		raise
+	return "Hello from Lambda"
